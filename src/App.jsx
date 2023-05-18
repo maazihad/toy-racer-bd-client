58e4ee4586e0eb1e155c240416369e45028a987e
maazihad <maazihadin@gmail.com>
2023-05-18 11:29:27 +0600

    ToyRacerBD client side is ready for starting project.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className='text-4xl text-red-600 font-bold italic text-center mt-52'>
+      <h2>Project client side is running...</h2>
+    </div>
+  );
+};
+
+export default App;
