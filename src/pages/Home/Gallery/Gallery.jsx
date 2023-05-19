8de2b984842b4e0d5b041ba1e92e261ea1d65432
maazihad <maazihadin@gmail.com>
2023-05-19 16:16:26 +0600

    In the gallery section data fetch from sever side and show the data gallery.

--- src/pages/Home/Gallery/Gallery.jsx
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+import GalleryCard from "./GalleryCard";
 
 const Gallery = () => {
-   return (
-      <div>
+   const [gallery, setGallery] = useState([]);
+   useEffect(() => {
+      fetch('http://localhost:5555/gallery', {
+         method: 'GET',
+         headers: {
+            'content-type': 'application/json'
+         },
+         body: JSON.stringify()
+      })
+         .then(res => res.json())
+         .then(data => setGallery(data));
+   }, []);
 
-      </div>
+   return (
+      <section className="bg-red-100 pt-5 pb-10">
+         <h2 className="text-5xl font-black py-5 text-center text-red-700">Toy-Racer Gallery</h2>
+         <div className="grid lg:grid-cols-4 gap-3 max-w-7xl mx-auto">
+            {
+               gallery.map(galleryCard => <GalleryCard
+                  key={galleryCard._id}
+                  galleryCard={galleryCard}
+               ></GalleryCard>)
+            }
+         </div>
+      </section>
    );
 };
 
---
+++ src/pages/Home/Gallery/GalleryCard.jsx
@@ -0,0 +1,14 @@
+
+const GalleryCard = ({ galleryCard }) => {
+   const { _id, img, name, price } = galleryCard || {};
+   return (
+      <div className="card bg-base-100 shadow-xl rounded-none">
+         <img className="object-cover h-56" src={img} alt="Shoes" />
+         <div className="p-5">
+            <p className="text-md text-black">{name}</p>
+         </div>
+      </div>
+   );
+};
+
+export default GalleryCard;
